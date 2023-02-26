f0fcd6aaa9d35f57114d84a3cedaa19ae91bbe9f
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-02-26 12:28:39 +0300

    Исправлены ошибки сборки в Task 2_1

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -1,21 +1,21 @@
 ﻿// 2. Напишите программу, которая выводит случайное число из отрезка 10,99 
 // и показывает наибольшую цифру числа.
 
-int Takenum(int num)
+int TakeNum(int num)
 {
     Console.WriteLine(num);
     int result1 = num % 10;
     int result2 = num / 10;
     if (result1 > result2)
     {
-        return result1
+        return result1;
     }
     else
     {
         return result2;
     }
-    Console.WriteLine(num);
-    return num % 10
+    //Console.WriteLine(num);
+    //return num % 10;
 }
 int result = TakeNum(new Random().Next(100, 1000));
 Console.WriteLine(result);
