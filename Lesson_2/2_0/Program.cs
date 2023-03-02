058323364543c601be12b3a3f41c5f4ce5b839f6
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-02 22:10:17 +0300

    Task 2_0 completed

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -1,14 +1,14 @@
 ﻿// 1. Напишите программу, которая на вход принимает трёхзначное число 
 // и на выходе показывает последнюю цифру этого числа.
 
-int Takenum(int num)
+int TakeNum(int num)
 {
-    Console.WriteLine(num);
-    return num % 10
+    Console.WriteLine("Число: "+num);
+    return num % 10;
 }
 // int num = new Random().Next(100, 1000);
 int result = TakeNum(new Random().Next(100, 1000));
-Console.WriteLine(result);
+Console.WriteLine("Последняя цифра: "+result);
 // Console.WriteLine(num);
 // получаем случайное трехзначное число
 // Console.WriteLine(num % 10);
