86d6654ca3faa82ec2b9df6e13ccfe2840e4e3b7
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-02 23:24:06 +0300

    Restored tasks in HW Lesson_1 in according with accepted HW

--- Lesson_1/HW/1_4_HW/Program.cs
+++ Lesson_1/HW/1_4_HW/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Чётные в ряду от 1 до N:");
+int eveN = 2;
+
+if (number > 1)
+{
+    while (eveN <= number)
+    {
+        Console.WriteLine(eveN + " ,");
+        eveN = (eveN + 2);
+    }
+}
