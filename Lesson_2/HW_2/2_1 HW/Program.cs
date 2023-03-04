655514e293532afebfbb7a19283ce3df93a24d7e
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-04 23:21:20 +0300

    Repared 1-st Task

--- Lesson_2/HW_2/2_1 HW/Program.cs
+++ Lesson_2/HW_2/2_1 HW/Program.cs
@@ -4,18 +4,11 @@
 // 782 -> 8
 // 918 -> 1
 
-//int TakeNum(int num)
-//{
-//    Console.WriteLine("Число: "+num);
-//    return num % 10;
-//}
-//int result = TakeNum(new Random().Next(100, 1000));
-//Console.WriteLine("Последняя цифра: "+result);
+Console.WriteLine("Последняя цифра: "+result);
 
 int TakeNum(int num)
 {
     Console.WriteLine("Число: "+num);
-//    num = num / 100;
     return num /10 % 10;   
 }
 int result = TakeNum(new Random().Next(100, 1000));
---
+++ Lesson_2/HW_2/2_3 HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int TakeNum(int (num))
+if (num < 100)
+{
+    Console.WriteLine("Число: " + num);
+    {
+        Console.Writeline("Третьей цифры нет");
+        return num % 10;
+    }
+}
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine("Третья цифра: " + result);
