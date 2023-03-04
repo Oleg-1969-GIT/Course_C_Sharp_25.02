adf5c172933f3793ee21953456f184bc73efd2df
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-04 09:59:46 +0300

    Переход на третью ветку

---
+++ Lesson_2/HW_2/2_2 HW/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int n = Console.ReadLine();
+
+object value1 = Console.WriteLine(n > 99 ? n.ToString()[2] : '-');
+object value = Console.ReadKey(true);
