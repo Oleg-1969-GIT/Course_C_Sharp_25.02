0f726f08156ff7f3a2ee65309e210c2859fd621a
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-05 11:52:11 +0300

    HW 3_1 comleted

--- Lesson_3/HW_3/3_1 HW/Program.cs
+++ Lesson_3/HW_3/3_1 HW/Program.cs
@@ -3,3 +3,18 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+string number = Console.ReadLine()!;
+void CheckNum(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Палиндром");
+    }
+    else Console.WriteLine($"НЕ палиндром");
+}
+if (number!.Length == 5)
+{
+    CheckNum(number);
+}
+else Console.WriteLine($"Это не пятизначное число");
