28cf77523f0871ef376e2a9e8e620ad9a65237db
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-11 22:47:19 +0300

    Class work 4 restored

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число N,
+// а на выходе выдает произведение чисел от 1 до N.
+
+int ProdNum(int num)
+{
+    int all_prod = 1;
+    for (int i = 1; i <= num; i++)
+       all_prod = i;
+    
+    return all_prod;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = ProdNum(A);
+Console.WriteLine(ProdNum(result));
---
+++ Lesson_4/4_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов
+// заполненный нулями и единичками в случайном порядке.
+// [0,1,0,0,0,1,1,0]
+
+void Massiv(int length)
+{
+    int[] R = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        R[i] = new Random().Next(2);
+        Console.Write($" " + R[i]);
+    }
+}
+Massiv(int.Parse(Console.ReadLine()!));
