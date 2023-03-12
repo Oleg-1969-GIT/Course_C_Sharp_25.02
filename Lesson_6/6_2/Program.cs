6bfbde3fc577c5b57182c32adb79359debaac4b7
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-12 11:01:43 +0300

    Task 6_2

--- Lesson_6/6_2/Program.cs
+++ Lesson_6/6_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать
+// десятичные число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string Binary(int num)
+{
+    string res = "";
+
+    while (num > 0)
+    {
+        res = num % 2 + res;
+        num /= 2;
+    }
+    return res;
+}
+
+// 12 - 1100, 4 - 100, 45 - 101101
+int n = int.Parse(Console.ReadLine()!);
+string result = Binary(n);
+Console.WriteLine(result);
