ec6db7028dc7719f5cdbada6cba257a479e8ac5a
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-12 10:01:38 +0300

    Created tasks for HW 5

---
+++ Lesson_4/HW_4/4_1_HW/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) 
+// и возводит число А в натуральную степень В.
+
+int A = Count("A");
+int B = Count("B");
+int Count(string countName)
+{
+    Console.Write($"Число {countName}:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Degree(int B)
+{
+    int a_degree = 1;
+    for (int i = 1; i <= B; i++)
+        a_degree = a_degree * A;
+    //A = a_degree;
+
+    return a_degree;
+}
+// int A = int.Parse(Console.ReadLine()!);
+int result = Degree(A);
+Console.WriteLine($" -> " + Degree(result));
