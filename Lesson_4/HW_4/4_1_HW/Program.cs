c611a249d9331314b5bbf367c4f183e02ecd1f1a
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-14 21:56:48 +0300

    HW 4_1 completed

---
+++ Lesson_4/HW_4/4_1_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243
+
+int Degree(int A, int B)
+{
+    int Step = 1;
+    for (int i = 0; i < B; i++)
+    {
+        Step *= A;
+    }
+    return Step;
+}
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($" {A} {B}  -> {Degree(A, B)}");
