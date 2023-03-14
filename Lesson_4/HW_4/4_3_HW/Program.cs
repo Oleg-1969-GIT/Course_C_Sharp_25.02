3e4c511e463965c1502e13246803df02b5d8fda6
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-14 22:51:37 +0300

    HW 4_3 - array with random N elements

---
+++ Lesson_4/HW_4/4_3_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции
+// (пригодится в следующих задачах)
+
+void Massiv(int length)
+{
+    int[] R = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        R[i] = new Random().Next(10);
+        Console.Write(" : " + R[i]);
+    }
+}
+Massiv(int.Parse(Console.ReadLine()!));
+Console.WriteLine();
