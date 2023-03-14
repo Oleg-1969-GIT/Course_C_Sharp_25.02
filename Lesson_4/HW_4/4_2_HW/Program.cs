755ad466342d19b9fec7798b841ee48d71ad4701
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-14 22:31:45 +0300

    HW 4_2 completed

--- Lesson_4/HW_4/4_2_HW/Program.cs
+++ Lesson_4/HW_4/4_2_HW/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 9012 -> 12
+
+int SumNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма всех цифр = {SumNum(num)}");
