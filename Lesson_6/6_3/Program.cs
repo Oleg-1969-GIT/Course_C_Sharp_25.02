c196de9c6f6aa9e61dc4e2eb1fff6cf68fd41059
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-14 20:54:20 +0300

    Task 6_3 Fibonacci

--- Lesson_6/6_3/Program.cs
+++ Lesson_6/6_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Не используя рекурсию, выведите первые N чисел последовательности Фибоначчи.
+//    Первые два числа Фибоначчи: 0 и 1.
+
+void Fibo (int num)
+{
+    int A = 0;
+    int B = 1;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write(A + " ");
+        (A, B) = (B, A + B );
+    }
+}
+int res = int.Parse(Console.ReadLine()!);
+Fibo (res);
