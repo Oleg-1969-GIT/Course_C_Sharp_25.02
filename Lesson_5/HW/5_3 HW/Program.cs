224142ef00ae5ac0126792e27d2956ee94a78d1d
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-18 20:41:47 +0300

    HW 5_3 completed - difference between max & min

--- Lesson_5/HW/5_3 HW/Program.cs
+++ Lesson_5/HW/5_3 HW/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементами массива.
 // [3 7 22 2 78] -> 76
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void MaxMinusMin(int[] arr)
+{
+    int min = arr[0];
+    int max = arr[0];
+    int diff = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+
+        else if (arr[i] > max)
+            max = arr[i];
+    }
+    Console.WriteLine($"Разница между максимальным и минимальным = " + (diff = max - min));
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+MaxMinusMin(mass);
