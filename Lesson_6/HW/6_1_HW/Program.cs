3650e595a843a5f31a566eba5e12c40bcc5e5fbb
Oleg-1969-GIT <125119933+Oleg-1969-GIT@users.noreply.github.com>
2023-03-18 22:21:03 +0300

    HW 6_1 ввод с консоли: кол-во чисел и чисел, вывод кол-ва больше 0

---
+++ Lesson_6/HW/6_1_HW/Program.cs
@@ -0,0 +1,48 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2, -> 2
+
+int Row(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Row($"Введите {i + 1} -й элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = Row("Введите кол-во элементов: ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Кол-во чисел > 0: {CountPositiveNumbers(array)}");
